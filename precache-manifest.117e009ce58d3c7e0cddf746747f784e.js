5d40873e12a50244917e73a07ca62204c74a5fbd
Rohit Jha <45750703+therohitjha@users.noreply.github.com>
2020-02-28 21:44:23 +0530

    Updates

---
+++ precache-manifest.117e009ce58d3c7e0cddf746747f784e.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8a7173785ec8f11af3293e71d2f0eaf",
+    "url": "/react-carousel/index.html"
+  },
+  {
+    "revision": "3660aaa76f67c3328393",
+    "url": "/react-carousel/static/css/2.cd690fd8.chunk.css"
+  },
+  {
+    "revision": "e929e7b853390f955a6a",
+    "url": "/react-carousel/static/css/main.96b07a21.chunk.css"
+  },
+  {
+    "revision": "3660aaa76f67c3328393",
+    "url": "/react-carousel/static/js/2.bde9d8c4.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/react-carousel/static/js/2.bde9d8c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e929e7b853390f955a6a",
+    "url": "/react-carousel/static/js/main.d0ed421f.chunk.js"
+  },
+  {
+    "revision": "0dc0e6268721aa6dcda1",
+    "url": "/react-carousel/static/js/runtime-main.95e6a7e0.js"
+  },
+  {
+    "revision": "dbabba7721fd6659dfe22ab94e65c5df",
+    "url": "/react-carousel/static/media/1.dbabba77.png"
+  },
+  {
+    "revision": "8ce28450a4c22ac9b8864b96dfc9773e",
+    "url": "/react-carousel/static/media/2.8ce28450.png"
+  },
+  {
+    "revision": "5fe8b5e359288345203a2f3d6e85eb29",
+    "url": "/react-carousel/static/media/3.5fe8b5e3.png"
+  },
+  {
+    "revision": "173ccb235dcb789323618b0430cf28d9",
+    "url": "/react-carousel/static/media/4.173ccb23.png"
+  },
+  {
+    "revision": "beace76ae3fdacd0b36ddff7c688f2ad",
+    "url": "/react-carousel/static/media/5.beace76a.png"
+  },
+  {
+    "revision": "5c77b40817bfef5a6180a359848da9ce",
+    "url": "/react-carousel/static/media/leftArrowWithCircle.5c77b408.svg"
+  },
+  {
+    "revision": "63c2876aaabe1bcb166998dd17878970",
+    "url": "/react-carousel/static/media/rightArrowWithCircle.63c2876a.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/react-carousel/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/react-carousel/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/react-carousel/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/react-carousel/static/media/slick.f97e3bbf.svg"
+  }
+]);
